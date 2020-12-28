4f6a71679da75d5527c3e0306cdbfca79138b5bd
RemasLover13 <55665862+RemasLover13@users.noreply.github.com>
2020-12-28 11:50:55 +0300

    Create constraint.sql

---
+++ constraint.sql
@@ -0,0 +1,17 @@
+ALTER TABLE PRODUCT
+    ADD CONSTRAINT positive_price CHECK ( price > 0 );
+
+ALTER TABLE PRODUCT
+    ADD CONSTRAINT less_when_number CHECK ( price > 500000 );
+ALTER TABLE CUSTOMER
+    ADD COLUMN email VARCHAR(50) NOT NULL default ('THIS.IS.IMPOSSIBLE@MAIL.RU');
+
+ALTER TABLE CUSTOMER
+    ADD CONSTRAINT unique_email UNIQUE (email);
+CREATE TABLE PASSPORT
+(
+    series varchar(4) CHECK ( char_length(series) = 4 ),
+    number varchar(6) CHECK ( char_length(number) = 6 ),
+    unique (series, number),
+    PRIMARY KEY (series, number)
+);
