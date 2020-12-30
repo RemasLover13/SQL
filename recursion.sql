005c7992b9aab82925bcd13dfdec416fb6ff08c2
RemasLover13 <55665862+RemasLover13@users.noreply.github.com>
2020-12-30 12:30:50 +0300

    Update recursion.sql

--- recursion.sql
+++ recursion.sql
@@ -1,23 +1,16 @@
-WITH RECURSIVE orders_statistics(customer_id, month, sum) AS
-(
-    SELECT orders.id_customer, date_trunc('month', orders.order_date), SUM(orders.cost)
-    FROM orders
-    INNER JOIN (
-        SELECT o1.id_customer, MIN(date_trunc('month', o1.order_date)) earliest_order_date_month
-        FROM orders o1
-        GROUP BY 1)
-        earliest_orders ON earliest_orders.earliest_order_date_month = date_trunc('month', orders.order_date)
-    GROUP BY 1, 2
-    UNION ALL
-    SELECT stat.customer_id, next_month_orders.order_date_month, stat.sum + next_month_orders.sum
-    FROM orders_statistics stat
-    INNER JOIN (
-        SELECT o.id_customer, date_trunc('month', o.order_date) order_date_month, SUM(o.cost) sum
-        FROM orders o
-        GROUP BY 1, 2)
-        next_month_orders ON next_month_orders.id_customer = stat.customer_id
-            AND next_month_orders.order_date_month = date_trunc('month', stat.month) + INTERVAL '1 month'
-    WHERE next_month_orders.id_customer IS NOT NULL
+WITH RECURSIVE r AS (
+    -- стартовая часть рекурсии (т.н. "anchor")
+    SELECT 
+        1 AS i, 
+        1 AS factorial
+    
+    UNION 
+    
+    -- рекурсивная часть 
+    SELECT 
+        i+1 AS i, 
+        factorial * (i+1) as factorial 
+    FROM r
+    WHERE i < 10
 )
-SELECT * FROM orders_statistics
-ORDER BY 1, 2;
+SELECT * FROM r;
